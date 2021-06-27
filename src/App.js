840ecf2297156de8fe1ddde142ab986b3075f3b2
abenitezicp <abenitez@icpayment.net>
2021-06-27 11:53:52 -0300

    Part 2 near end, just need details

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Students from './components/Students';
-
 
 function App() {
   const url = 'https://api.hatchways.io/assessment/students';
   const [students, setStudents] = useState();
   const fetchApi = async () => {
-    const response = await fetch(url);    
+    const response = await fetch(url);
     const responseJSON = await response.json();
-    const data = responseJSON.students;    
-    setStudents(data);    
-  }
+    const data = responseJSON.students;
+    setStudents(data);
+  };
   useEffect(() => {
-    fetchApi()
-  }, [])
+    fetchApi();
+  }, []);
   return (
     <div className="App">
-
-         { !students ? 'Cargando...' :
-          students.map( (student, index)=>{
-            return <Students name={student.firstName} lastName={student.lastName} pic={student.pic} email={student.email} company={student.company} skill={student.skill} grades={student.grades} />
-          })
-         }
-      
+      {!students
+        ? 'Cargando...'
+        : students.map((student) => {
+            return (
+              <Students
+                name={student.firstName}
+                lastName={student.lastName}
+                pic={student.pic}
+                email={student.email}
+                company={student.company}
+                skill={student.skill}
+                grades={student.grades}
+              />
+            );
+          })}
     </div>
   );
 }
--- src/components/Students.js
+++ src/components/Students.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import './Students.css';
+import PropTypes from 'prop-types';
 
+function Students({ name, pic, lastName, grades, email, company, skill }) {
+  const data = grades.reduce((beforeValue, afterValue) => {
+    return Number(beforeValue) + Number(afterValue);
+  });
+  const result = data / grades.length || 0;
+  const average = result.toFixed(3);
 
-function Students(props) {
-
-    const grades = props.grades;
-    const data = grades.reduce(function(valorAnterior, valorActual){
-      return Number(valorAnterior) + Number(valorActual);
-    });
-    const result = (data / grades.length) || 0;
-    const average = result.toFixed(3);
-   
-    return (
-        <div className="UserInfo">
-        <img className="Avatar"
-        src={props.pic}
-        alt={props.name}
-      />
-        <div className="UserInfo-name UserInfo-lastName">
-          <b>{props.name} {props.lastName} </b>
+  return (
+    <div className="UserInfo" id="flex-container">
+      <div id="avatar">
+        <img className="Avatar" src={pic} alt={name} />
+      </div>
+      <div id="info">
+        <div id="nameTitle">
+          <div className="UserInfo-name UserInfo-lastName">
+            <b>
+              {name} {lastName}
+            </b>
+          </div>
         </div>
-        <div className="UserInfo-email">
-         Email: {props.email} 
-        </div>
-        <div className="UserInfo-company">
-         Company: {props.company} 
-        </div>
-        <div className="UserInfo-skill">
-         Skill: {props.skill} 
-        </div>
-        <div className="UserInfo-average">
-         Average: {average}%
+        <div id="data">
+          <div className="UserInfo-email">Email: {email}</div>
+          <div className="UserInfo-company">Company: {company}</div>
+          <div className="UserInfo-skill">Skill: {skill}</div>
+          <div className="UserInfo-average">Average: {average}%</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Students;
+Students.propTypes = {
+  grades: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  skill: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+};
+
+export default Students;
